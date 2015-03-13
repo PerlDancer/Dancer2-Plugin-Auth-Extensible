4820be37d4f01ee9799de88ca2f1e4a2b6570e4d
Andrew Beverley <a.beverley@ctrlo.com>
2015-03-13 17:43:37 +0000

    Force username to be specified with update_user

--- lib/Dancer2/Plugin/Auth/Extensible.pm
+++ lib/Dancer2/Plugin/Auth/Extensible.pm
@@ -499,49 +499,33 @@ register authenticate_user => \&authenticate_user;
 Updates a user's details. If the authentication provider supports it, this
 keyword allows a user's details to be updated within the backend data store.
 
-In order to update the user's details, the keyword should be called with a
-hash of the values to be updated. Note that whilst the password can be
-updated using this method, any new value will be stored directly into the
-provider as-is, not encrypted. It is recommended to use L<user_password>
-instead.
+In order to update the user's details, the keyword should be called with the
+username to be updated, followed by a hash of the values to be updated. Note
+that whilst the password can be updated using this method, any new value will
+be stored directly into the provider as-is, not encrypted. It is recommended to
+use L<user_password> instead.
 
-By default the current logged in user is updated. However, if a different
-user is to be updated, then specify a username key and value as the first
-parameter. In that case, the realm also needs to be specified using the key
-C<realm>. To update the username, specify its new value as a second username
-key.
+If only one realm is configured then this will be used to search for the user.
+Otherwise, the realm must be specified with the realm key.
 
-    # Update current user
-    update_user surname => "Smith"
+    # Update user, only one realm configured
+    update_user "jsmith", surname => "Smith"
 
-    # Update specific user, not necessarily logged in
-    update_user username => "jsmith", realm => "dbic", surname => "Smith"
-
-    # Update a specific user's username
-    update_user username => "jsmith", realm => "dbic", username => "jjones"
+    # Update a user's username, more than one realm
+    update_user "jsmith", realm => "dbic", username => "jjones"
 
 The updated user's details are returned, as per L<logged_in_user>.
 
 =cut
 
 sub update_user {
-    my $dsl     = shift;
+    my ($dsl, $username, %update) = @_;
 
-    # Is a different user specified for the update?
-    my $username; my $realm; my %update;
-    if ($_[0] eq 'username') {
-        $username = shift && shift;
-        %update   = @_;
-        $realm    = delete $update{realm}
-            or die "User's realm needs to be specified when using username for update";
-    } else {
-        %update     = @_;
-        my $session = $dsl->app->session;
-        $username   = $session->read('logged_in_user')
-            or die "No user currently logged in to update";
-        $realm      = $session->read('logged_in_user_realm');
-    }
+    my @all_realms = keys %{ $settings->{realms} };
+    die "Realm must be specified when more than one realm configured"
+        if !$update{realm} && @all_realms > 1;
 
+    my $realm    = delete $update{realm} || $all_realms[0];
     my $provider = auth_provider($dsl, $realm);
     $provider->set_user_details($username, %update);
 }
