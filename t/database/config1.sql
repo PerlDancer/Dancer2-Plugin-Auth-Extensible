f7b920de20c97b6a3c88f267f3550b5e9747e465
Peter Mottram <peter@sysnix.com>
2016-03-31 12:43:52 +0200

    remove Dancer2::Plugin::Auth::Extensible::Provider::Database
    This has moved to its own repo and will be released as a standalone
    distro.

--- t/database/config1.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users VALUES (1, 'dave', 'beer', 'David Precious'), (2, 'bob', 'cider', 'Bob Smith'), (3, 'mark', 'wantscider', 'Update here');
-INSERT INTO roles VALUES (1, 'BeerDrinker'), (2, 'Motorcyclist'), (3, 'CiderDrinker');
-INSERT INTO user_roles VALUES (1,1), (1,2), (2,3);
--- t/database/config2.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO users VALUES (1, 'burt', 'bacharach', ''), (2, 'hashedpassword', '{SSHA}+2u1HpOU7ak6iBR6JlpICpAUvSpA/zBM', ''), (3, 'mark', 'wantscider', '');
