25c79751bcfa4b520d7ddc7784b28a17473428d6
Peter Mottram (SysPete) <peter@sysnix.com>
2016-06-18 17:58:01 +0200

    Config provider should needs user and pass attributes
    This is for backwards compatibility with code still expecting a hash of
    those.

--- lib/Dancer2/Plugin/Auth/Extensible/Provider/Config.pm
+++ lib/Dancer2/Plugin/Auth/Extensible/Provider/Config.pm
@@ -10,16 +10,36 @@ our $VERSION = '0.613';
 
 {
     package User;
+    use Dancer2::Core::Types qw/Str/;
     use Moo;
     with "Dancer2::Plugin::Auth::Extensible::Role::User";
     use namespace::clean;
 
+    # We need the underlying hash to have user/pass for backward-compatibility
+    # with code that expects these two rather than username/password of
+    # a normal User object. At some point we'll add deprecation via
+    # a warning in BUILD.
+
+    has user => (
+        is       => 'ro',
+        isa      => Str,
+        required => 1,
+    );
+
     has '+username' => (
-        init_arg => 'user',
+        lazy => 1,
+        default => sub { $_[0]->user },
+    );
+
+    has pass => (
+        is       => 'ro',
+        isa      => Str,
+        required => 1,
     );
 
     has '+password' => (
-        init_arg => 'pass',
+        lazy => 1,
+        default => sub { $_[0]->pass },
     );
 }
 
