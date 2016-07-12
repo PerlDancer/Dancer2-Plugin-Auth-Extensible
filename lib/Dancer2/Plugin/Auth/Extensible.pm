1875568153e2e1d71066d67f896330698ddf23f4
Stefan Hornburg (Racke) <racke@linuxia.de>
2016-07-12 16:23:00 +0200

    Bump up version number.

--- lib/Dancer2/Plugin/Auth/Extensible.pm
+++ lib/Dancer2/Plugin/Auth/Extensible.pm
@@ -1,6 +1,6 @@
 package Dancer2::Plugin::Auth::Extensible;
 
-our $VERSION = '0.600';
+our $VERSION = '0.601';
 
 use strict;
 use warnings;
--- lib/Dancer2/Plugin/Auth/Extensible/Test.pm
+++ lib/Dancer2/Plugin/Auth/Extensible/Test.pm
@@ -1,6 +1,6 @@
 package Dancer2::Plugin::Auth::Extensible::Test;
 
-our $VERSION = '0.600';
+our $VERSION = '0.601';
 
 =head1 NAME
 
