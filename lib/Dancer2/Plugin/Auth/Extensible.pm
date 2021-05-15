5993df37ccf774513fad5a7c48175e78bcad954d
Andy Beverley <a.beverley@ctrlo.com>
2021-05-15 08:01:27 +0100

    Release 709

--- lib/Dancer2/Plugin/Auth/Extensible.pm
+++ lib/Dancer2/Plugin/Auth/Extensible.pm
@@ -1,6 +1,6 @@
 package Dancer2::Plugin::Auth::Extensible;
 
-our $VERSION = '0.708';
+our $VERSION = '0.709';
 
 use strict;
 use warnings;
--- lib/Dancer2/Plugin/Auth/Extensible/Test.pm
+++ lib/Dancer2/Plugin/Auth/Extensible/Test.pm
@@ -1,6 +1,6 @@
 package Dancer2::Plugin::Auth::Extensible::Test;
 
-our $VERSION = '0.708';
+our $VERSION = '0.709';
 
 =head1 NAME
 
